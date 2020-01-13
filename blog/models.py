f10953701d9cf0ad74dcbfae7b5fab2c6c85edc3
yung-ting-chang <tina.yt.chang@protonmail.com>
2020-01-13 15:33:27 +0100

    modified tempaltes to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,8 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+
 
     def publish(self):
         self.published_date = timezone.now()
